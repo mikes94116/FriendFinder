92b41a828b648845024844dc4cb67c478f3e54ae
mathpro2018 <mikessvk@yahoo.com>
2018-11-19 21:16:44 -0800

    pushin

---
+++ app/data/friend.js
@@ -0,0 +1,22 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+  {
+    name: 'Mike Sevik',
+    profilePic: 'https://avatars1.githubusercontent.com/u/42045953?s=400&u=e073a582a672523dcdb75dc2d012c028660ba239&v=4',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
